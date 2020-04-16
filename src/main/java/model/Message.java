0a56cb1d67775d8a0de2729217aefe2c3f36e228
samurai <1214737761@qq.com>
2020-04-15 17:31:57 -0700

    package name

--- src/main/java/model/Message.java
+++ src/main/java/model/Message.java
@@ -1,5 +1,6 @@
 package model;
 
+
 import java.io.Serializable;
 
 public class Message implements Serializable {
@@ -13,6 +14,19 @@ public class Message implements Serializable {
     this.sender = sender;
     this.content = content;
     this.timestamp = timestamp;
+  }
+
+  public byte[] getBytes() {
+    String s = timestamp + ":" + sender + " " + content;
+    return s.getBytes();
+  }
+
+  public Message(String string) {
+    String[] timeHead = string.split("/", 2);
+    this.timestamp = Long.parseLong(timeHead[0]);
+    String[] senderHead = timeHead[1].split(" ", 2);
+    this.sender = senderHead[0];
+    this.content = senderHead[1];
   }
 
   public static long getSerialVersionUID() {
